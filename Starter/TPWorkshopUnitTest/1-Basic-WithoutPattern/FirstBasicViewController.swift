c693791f94975f1c78776c9686cfb04fb60e738e
Anang Nugraha <anangnugraha8@gmail.com>
2022-05-17 23:43:12 +0700

    Stage 1 Finished

--- Starter/TPWorkshopUnitTest/1-Basic-WithoutPattern/FirstBasicViewController.swift
+++ Starter/TPWorkshopUnitTest/1-Basic-WithoutPattern/FirstBasicViewController.swift
@@ -22,12 +22,60 @@ class FirstBasicViewController: UIViewController {
     }
 
     @IBAction func didTapFindOddEven(_ sender: Any) {
-       
+        guard let oddEvenText = oddEvenTextField.text, !oddEvenText.isEmpty, let oddEvenNumber = Int(oddEvenText) else { return }
+        
+        if isEven(number: oddEvenNumber) {
+            oddEvenResultLabel.text = "Genap"
+        } else {
+            oddEvenResultLabel.text = "Ganjil"
+        }
     }
+    
+    func isEven(number: Int) -> Bool {
+        if number % 2 == 0 {
+            return true
+        }
+        
+        return false
+    }
+    
+    func numberFormatter(number: Int, separator: String = ".", currency: String = "Rp") -> String {
+          guard number > 0 else {
+              return "\(currency) \(number)"
+          }
+
+          var copyNumber = number
+          var result: [String] = []
+          while copyNumber > 0 {
+              let lastNumber = copyNumber % 1000
+              copyNumber = copyNumber / 1000
+
+              if lastNumber == 0 {
+                  result.insert("000", at: 0)
+              } else if lastNumber < 10 && copyNumber > 0 {
+                  result.insert("00\(lastNumber)", at: 0)
+              } else if lastNumber < 100 && copyNumber > 0 {
+                  result.insert("0\(lastNumber)", at: 0)
+              } else {
+                  result.insert("\(lastNumber)", at: 0)
+              }
+
+              if copyNumber > 0 {
+                  result.insert(separator, at: 0)
+              }
+          }
+
+          return "\(currency) \(result.joined())"
+      }
 }
 
 extension FirstBasicViewController: UITextFieldDelegate {
     func textFieldDidChangeSelection(_ textField: UITextField) {
-        
+        guard let numberFormatterText = textField.text, !numberFormatterText.isEmpty, let number = Int(numberFormatterText) else {
+            numberFormatterResultLabel.text = "Not number"
+            return
+        }
+
+        numberFormatterResultLabel.text = numberFormatter(number: number)
     }
 }
--- Starter/TPWorkshopUnitTestTests/1-Basic-WithoutPattern.swift
+++ Starter/TPWorkshopUnitTestTests/1-Basic-WithoutPattern.swift
@@ -18,4 +18,49 @@ class __Basic_WithoutPattern: XCTestCase {
     override func tearDown() {
         vc = nil
     }
+    
+    func test_ifEven_odd() {
+        let isOdd = vc.isEven(number: 15)
+        XCTAssertFalse(isOdd)
+    }
+    
+    func test_ifEven_even() {
+        let isEven = vc.isEven(number: 10)
+        XCTAssertTrue(isEven)
+    }
+    
+    func test_numberFormatter_tens() {
+        let formattedNumber = vc.numberFormatter(number: 17, separator: ".", currency: "Rp")
+        XCTAssertEqual(formattedNumber, "Rp 17")
+    }
+    
+    func test_numberFormatter_hundred() {
+        let formattedNumber = vc.numberFormatter(number: 170, separator: ".", currency: "Rp")
+        XCTAssertEqual(formattedNumber, "Rp 170")
+    }
+    
+    func test_numberFormatter_thousand() {
+        let formattedNumber = vc.numberFormatter(number: 1700, separator: ".", currency: "Rp")
+        XCTAssertEqual(formattedNumber, "Rp 1.700")
+    }
+    
+    func test_numberFormatter_ten_thousand() {
+        let formattedNumber = vc.numberFormatter(number: 17430, separator: ".", currency: "Rp")
+        XCTAssertEqual(formattedNumber, "Rp 17.430")
+    }
+    
+    func test_numberFormatter_hundred_thousand() {
+        let formattedNumber = vc.numberFormatter(number: 117430, separator: ".", currency: "Rp")
+        XCTAssertEqual(formattedNumber, "Rp 117.430")
+    }
+    
+    func test_numberFormatter_milion() {
+        let formattedNumber = vc.numberFormatter(number: 1717430, separator: ".", currency: "Rp")
+        XCTAssertEqual(formattedNumber, "Rp 1.717.430")
+    }
+    
+    func test_numberFormatter_dollar() {
+        let formattedNumber = vc.numberFormatter(number: 234523543, separator: ".", currency: "$")
+        XCTAssertEqual(formattedNumber, "$ 234.523.543")
+    }
 }
