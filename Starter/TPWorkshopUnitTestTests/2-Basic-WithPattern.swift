4bbcfbe522a99b08fbad59978d09a248562522da
Anang Nugraha <anangnugraha8@gmail.com>
2022-05-18 11:44:02 +0700

    completing stage 2 and starting stage 3

--- Starter/TPWorkshopUnitTestTests/2-Basic-WithPattern.swift
+++ Starter/TPWorkshopUnitTestTests/2-Basic-WithPattern.swift
@@ -9,5 +9,42 @@ import XCTest
 @testable import TPWorkshopUnitTest
 
 class __Basic_WithPattern: XCTestCase {
+    var viewModel: SecondBasicViewModel!
+    
+    override func setUp() {
+        viewModel = SecondBasicViewModel()
+    }
+    
+    func test_isEven_odd() {
+        let expect = expectation(description: "check if isEven returning Ganjil")
+        viewModel.resultOddEven = { result in
+            XCTAssertEqual(result, "Ganjil")
+            expect.fulfill()
+        }
+        
+        viewModel.isEven(text: "33")
+        waitForExpectations(timeout: 1)
+    }
+    
+    func test_isEven_even() {
+        let expect = expectation(description: "check if isEven returning Genap")
+        viewModel.resultOddEven = { result in
+            XCTAssertEqual(result, "Genap")
+            expect.fulfill()
+        }
+        
+        viewModel.isEven(text: "38")
+        waitForExpectations(timeout: 1)
+    }
+    
+    func test_numberFormatter_tens_withZero() {
+        let expect = expectation(description: "number formatter return tens with zero")
+        viewModel.resultNumberFormatter = { result in
+            XCTAssertEqual(result, "Rp 109.787")
+            expect.fulfill()
+        }
 
+        viewModel.numberFormatter(text: "109787")
+        waitForExpectations(timeout: 1, handler: nil)
+    }
 }
--- Starter/TPWorkshopUnitTestTests/3-Advanced.swift
+++ Starter/TPWorkshopUnitTestTests/3-Advanced.swift
@@ -10,4 +10,19 @@ import XCTest
 
 class __Advanced: XCTestCase {
     
+    func test_positive_didLoad() {
+        let useCase = MockPositiveWorkshopProvider()
+        let viewModel = AdvancedViewModel(usecase: useCase)
+        let expectation = expectation(description: "should return product data")
+        let resultExpectation = MockProductData.generateProductResult().data
+
+        viewModel.receiveData = {
+            XCTAssertEqual(viewModel.products, resultExpectation)
+            expectation.fulfill()
+        }
+        
+        viewModel.didLoad()
+        waitForExpectations(timeout: 1)
+    }
+    
 }
